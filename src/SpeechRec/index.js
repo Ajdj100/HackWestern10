8898f5081cf5ba57d8a030d7c92f6829757ea554
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-11-26 03:57:09 -0500

    reset to original version

--- src/SpeechRec/index.js
+++ src/SpeechRec/index.js
@@ -1,110 +1,113 @@
 // required dom elements
 const buttonEl = document.getElementById("button");
-const messageEl = document.getElementById("transcript");
+const messageEl = document.getElementById("message");
 const titleEl = document.getElementById("real-time-title");
 
 // set initial state of application variables
-// messageEl.style.display = "none";
+messageEl.style.display = "none";
 let isRecording = false;
 let socket;
 let recorder;
 
-//stops the audio transcription
-const stop = async () => {
-  console.log("STOPPING TRANSCRIPTION");
-  if (socket) {
-    socket.send(JSON.stringify({ terminate_session: true }));
-    socket.close();
-    socket = null;
-  }
-
-  if (recorder) {
-    recorder.pauseRecording();
-    recorder = null;
-  }
-}
-
 // runs real-time transcription and handles global variables
 const run = async () => {
-  const response = await fetch("http://localhost:8000"); // get temp session token from server.js (backend)
-  const data = await response.json();
-  console.log("hello");
-  if (data.error) {
-    alert(data.error);
-    return;
-  }
-  console.log(data);
-  const { token } = data;
-
-  // establish wss with AssemblyAI (AAI) at 16000 sample rate
-  socket = await new WebSocket(
-    `wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&token=${token}`,
-  );
-
-  // handle incoming messages to display transcription to the DOM
-  const texts = {};
-  socket.onmessage = (message) => {
-    let msg = "";
-    const res = JSON.parse(message.data);
-    texts[res.audio_start] = res.text;
-    const keys = Object.keys(texts);
-    keys.sort((a, b) => a - b);
-    for (const key of keys) {
-      if (texts[key]) {
-        msg += ` ${texts[key]}`;
-      }
+  if (isRecording) {
+    if (socket) {
+      socket.send(JSON.stringify({ terminate_session: true }));
+      socket.close();
+      socket = null;
     }
-    messageEl.innerText = msg;
-  };
 
-  socket.onerror = (event) => {
-    console.error(event);
-    socket.close();
-  };
+    if (recorder) {
+      recorder.pauseRecording();
+      recorder = null;
+    }
+  } else {
+    const response = await fetch("http://localhost:8000"); // get temp session token from server.js (backend)
+    const data = await response.json();
 
-  socket.onclose = (event) => {
-    console.log(event);
-    socket = null;
-  };
+    if (data.error) {
+      alert(data.error);
+      return;
+    }
+    console.log(data);
+    const { token } = data;
 
-  socket.onopen = () => {
-    // once socket is open, begin recording
-    // messageEl.style.display = "";
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((stream) => {
-        recorder = new RecordRTC(stream, {
-          type: "audio",
-          mimeType: "audio/webm;codecs=pcm", // endpoint requires 16bit PCM audio
-          recorderType: StereoAudioRecorder,
-          timeSlice: 250, // set 250 ms intervals of data that sends to AAI
-          desiredSampRate: 16000,
-          numberOfAudioChannels: 1, // real-time requires only one channel
-          bufferSize: 16384,
-          audioBitsPerSecond: 128000,
-          ondataavailable: (blob) => {
-            const reader = new FileReader();
-            reader.onload = () => {
-              const base64data = reader.result;
+    // establish wss with AssemblyAI (AAI) at 16000 sample rate
+    socket = await new WebSocket(
+      `wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&token=${token}`,
+    );
 
-              // audio data must be sent as a base64 encoded string
-              if (socket) {
-                socket.send(
-                  JSON.stringify({
-                    audio_data: base64data.split("base64,")[1],
-                  }),
-                );
-              }
-            };
-            reader.readAsDataURL(blob);
-          },
-        });
+    // handle incoming messages to display transcription to the DOM
+    const texts = {};
+    socket.onmessage = (message) => {
+      let msg = "";
+      const res = JSON.parse(message.data);
+      texts[res.audio_start] = res.text;
+      const keys = Object.keys(texts);
+      keys.sort((a, b) => a - b);
+      for (const key of keys) {
+        if (texts[key]) {
+          msg += ` ${texts[key]}`;
+        }
+      }
+      messageEl.innerText = msg;
+    };
 
-        recorder.startRecording();
-      })
-      .catch((err) => console.error(err));
-  };
+    socket.onerror = (event) => {
+      console.error(event);
+      socket.close();
+    };
+
+    socket.onclose = (event) => {
+      console.log(event);
+      socket = null;
+    };
+
+    socket.onopen = () => {
+      // once socket is open, begin recording
+      messageEl.style.display = "";
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
+          recorder = new RecordRTC(stream, {
+            type: "audio",
+            mimeType: "audio/webm;codecs=pcm", // endpoint requires 16bit PCM audio
+            recorderType: StereoAudioRecorder,
+            timeSlice: 250, // set 250 ms intervals of data that sends to AAI
+            desiredSampRate: 16000,
+            numberOfAudioChannels: 1, // real-time requires only one channel
+            bufferSize: 16384,
+            audioBitsPerSecond: 128000,
+            ondataavailable: (blob) => {
+              const reader = new FileReader();
+              reader.onload = () => {
+                const base64data = reader.result;
+
+                // audio data must be sent as a base64 encoded string
+                if (socket) {
+                  socket.send(
+                    JSON.stringify({
+                      audio_data: base64data.split("base64,")[1],
+                    }),
+                  );
+                }
+              };
+              reader.readAsDataURL(blob);
+            },
+          });
+
+          recorder.startRecording();
+        })
+        .catch((err) => console.error(err));
+    };
+  }
+
+  isRecording = !isRecording;
+  buttonEl.innerText = isRecording ? "Stop" : "Record";
+  titleEl.innerText = isRecording
+    ? "Click stop to end recording!"
+    : "Click start to begin recording!";
 };
 
-// startButton.addEventListener("click", () => run());
-// endButton.addEventListener("click", () => stop());
+buttonEl.addEventListener("click", () => run());
