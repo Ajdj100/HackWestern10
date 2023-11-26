edbb8edb9f55c304ff3a399cfcb00c1a58380f5d
Lisa Nguyen <lisatramnguyen@yahoo.ca>
2023-11-26 02:56:59 -0500

    Created idle page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         titleBar: '#0A191F',
         background: '#FBFBFB',
         darkBackground: '#E9E9E9',
+        brandColor: "#F886D1",
+        UIGreen: "#18B758",
+        UIGreenHover: "#169B4B",
+        UIRed: "#BF0B0B",
       }
     },
   },
